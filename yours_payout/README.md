63201cc3ee8d9bd977d0389682c9e1535c761f93
Corey Petty <corey.a.petty@gmail.com>
2016-04-24 12:25:33 -0300

    Created new routine based on mixed normal distributions.

--- yours_payout/README.md
+++ yours_payout/README.md
@@ -1,6 +1,8 @@
 **Payout Model for Yours Network**
 
-The `get_payouts` function generates the data to be plot using the matplotlib and plotly package
+There are two functions that generate the data to be plot using the matplotlib and plotly package.  The first, 
+a simple model, is based on a uniform distribution, and can be called using the `get_payouts_uniform` routine.  
+It can be described as follows
 
 Inputs:
 - `max_tip`: Maximum tip given
@@ -20,6 +22,22 @@ The following is also printed to screen when executing the `get_payouts` routine
 - Yours revenue
 - Creator revenue
 - Curator revenue
+
+The second routine, a more advanced and realistic model, is based on a mixture of normal distributions.  
+This model separates the curators into two groups:  A large group of small tippers, and a small group of large tippers.
+This routine is named `get_payouts_mix_normal`.
+
+Inputs:
+- `tip_amounts`: list of tip amounts.  The last value will be the "high tipper" value.
+- `num_tips`: number of tips
+- `curator_payout_perc`: Percentage of tips paid out to curators
+- `yours_payout_perc`: Percentage of tips paid to the Yours Network
+
+The normal distributions are more sharply peaked around the "small tipper" amounts (sigma of 0.1) and make up 90% of
+`num_tips`.  The remaining 10% of `num_tips`, is centered around the "high tipper" amount, and is broadly peaked 
+(sigma of 0.8).
+
+Everything else is the same as `get_payouts_uniform`. 
 
 Package Requirements
 - pandas
---
+++ yours_payout/model_defs.py
@@ -0,0 +1,65 @@
+__author__ = 'Corey Petty'
+import pandas as pd
+import numpy as np
+
+
+def get_payouts_uniform(max_tip, num_tips, curator_payout_perc, yours_payout_perc) -> pd.DataFrame:
+    tips = np.random.uniform(0.00001, max_tip, num_tips)
+    curator_payouts = [list() for _ in range(0, num_tips)]
+    yours_payouts = tips * yours_payout_perc
+    for current_tipper, current_tip in enumerate(tips, 1):
+        distribution_total = sum(tips[:current_tipper-1])
+        for payout_num in range(0, current_tipper-1):
+            payout_tip_perc = tips[payout_num] / distribution_total
+            curator_payouts[payout_num].append(current_tip * curator_payout_perc * payout_tip_perc)
+
+    df = pd.DataFrame([tips, curator_payouts, yours_payouts])
+    df = df.transpose()
+    df.columns = ['tip', 'curator_payouts', 'yours_revenue']
+    df['curator_num'] = [x for x in range(num_tips)]
+    df['total_earned'] = [sum(x) for x in df.curator_payouts]
+
+    # Print some stats to screen
+    print('Total tipped:    ${0:.2f}'.format(round(df.tip.sum(), 2)))
+    print('Yours revenue:   ${0:.2f}'.format(round(df.yours_revenue.sum(), 2)))
+    print('Creator revenue: ${0:.2f}'.format(round(df.tip[1:].sum() * (1 - (yours_payout_perc + curator_payout_perc))
+                                                   + df.tip[0] * (1 - yours_payout_perc), 2)))
+    print('Curator revenue: ${0:.2f}'.format(round(df.total_earned.sum(), 2)))
+    return df
+
+
+def get_payouts_mixed_normal(tip_amounts, num_tips, curator_payout_perc, yours_payout_perc) -> pd.DataFrame:
+    # Create mixed set of normal distributions, centered around each tip amount provided
+    #   in tip_amounts.
+    #   Assumed that last tip amount is an outlier, will be 10% of num_tips,
+    #   and be more spread out than other distributions
+    sigmas = [0.1 for _ in range(len(tip_amounts))]
+    sigmas[-1] = 0.8                # spread out outlier tips
+    sizes = [(0.9 * num_tips) / (len(tip_amounts) - 1) for _ in range(len(tip_amounts))]
+    sizes[-1] = 0.1 * num_tips
+    distributions = [np.random.normal(mu, sigma, int(size)) for mu, sigma, size in zip(tip_amounts, sigmas, sizes)]
+    tips = np.concatenate(distributions)
+    # Randomize the tips
+    np.random.shuffle(tips)
+
+    curator_payouts = [list() for _ in range(0, num_tips)]
+    yours_payouts = tips * yours_payout_perc
+    for current_tipper, current_tip in enumerate(tips, 1):
+        distribution_total = sum(tips[:current_tipper-1])
+        for payout_num in range(0, current_tipper-1):
+            payout_tip_perc = tips[payout_num] / distribution_total
+            curator_payouts[payout_num].append(current_tip * curator_payout_perc * payout_tip_perc)
+
+    df = pd.DataFrame([tips, curator_payouts, yours_payouts])
+    df = df.transpose()
+    df.columns = ['tip', 'curator_payouts', 'yours_revenue']
+    df['curator_num'] = [x for x in range(num_tips)]
+    df['total_earned'] = [sum(x) for x in df.curator_payouts]
+
+    # Print some stats to screen
+    print('Total tipped:    ${0:.2f}'.format(round(df.tip.sum(), 2)))
+    print('Yours revenue:   ${0:.2f}'.format(round(df.yours_revenue.sum(), 2)))
+    print('Creator revenue: ${0:.2f}'.format(round(df.tip[1:].sum() * (1 - (yours_payout_perc + curator_payout_perc))
+                                                   + df.tip[0] * (1 - yours_payout_perc), 2)))
+    print('Curator revenue: ${0:.2f}'.format(round(df.total_earned.sum(), 2)))
+    return df
