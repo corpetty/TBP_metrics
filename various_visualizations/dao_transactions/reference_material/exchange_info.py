b637b1c5a8a9b04c2df1d6c09b8ce1f0d6641048
Corey Petty <cryptocmd@gmail.com>
2016-06-13 14:08:39 -0300

    Moved dao_transactions to new repo

--- various_visualizations/dao_transactions/reference_material/exchange_info.py
+++
@@ -1,8 +0,0 @@
-exchanges = {
-    'poloniex': '0xb794f5ea0ba39494ce839613fffba74279579268',
-    'polo_2': '0x32be343b94f860124dc4fee278fdcbd38c102d88',
-    'polo_new': '0xDf21fA922215B1a56f5a6D6294E6E36c85A0Acfb',
-    'kraken': '0x2910543af39aba0cd09dbb2d50200b3e800a63d2',
-    'bitfinex': '0xcafb10ee663f465f9d10588ac44ed20ed608c11e',
-    'gatecoin_hacked': '0x40b9b889a21ff1534d018d71dc406122ebcf3f5a',
-}
